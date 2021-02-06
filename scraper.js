34f1d0dc87ce4631899d29f27441c7fa8b64c9e7
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-06 18:02:57 -0300

    Add comments

--- scraper.js
+++ scraper.js
@@ -8,14 +8,20 @@ const random_useragent = require('random-useragent')
 const { url } = require('./config')
 
 ;(async () => {
+  // Open browser
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
+
+  // Setup browser
   await page.setDefaultTimeout(10000)
   await page.setDefaultNavigationTimeout(20000)
   await page.setViewport({ width: 1368, height: 768 })
   await page.setUserAgent(random_useragent.getRandom())
+
+  // Get data from bookstore
   await page.goto(url)
 
+  // Open browser
   await browser.close()
 })().catch((error) => {
   console.log(error)
